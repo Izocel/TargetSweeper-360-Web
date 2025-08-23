d0be2d1a292177d8573129a2fad7420a6a6f3546
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2025-08-23 14:16:04 -0400

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/TargetSweeper-360-Web/', // Set base for GitHub Pages
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 4173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/data': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
   },
   preview: {
     host: '0.0.0.0',
